842bf85c9b1ce4fb68668773b0ab2a62ee740bb6
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2023-11-30 08:51:58 +0100

    first start solve

--- 2023/01/solve.hs
+++ 2023/01/solve.hs
@@ -1,2 +1,17 @@
+import Data.Char (isDigit)
+import System.Posix (fileAccess)
+
+main :: IO ()
 main = do
-    putStr "Hello world"
+  putStrLn "Hello AoC"
+  content <- readFile "input.txt"
+  print $ sum $ map extractCode $ lines content
+
+extractDigits :: String -> String
+extractDigits = filter isDigit
+
+extractFirstLast :: String -> String
+extractFirstLast str = [head str, last str]
+
+extractCode :: String -> Int
+extractCode str = read $ extractFirstLast $ extractDigits str
