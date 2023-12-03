ff74b9ec93f71d18d4f979a29ab3ef1f1a47d66f
Nicholas Zivkovic <niki.zivkovic@hotmail.com>
2023-12-03 13:39:57 +0100

    day 3 part 2 done
    - long live python, it's so fast, to code

--- 2023/03/solve.py
+++ 2023/03/solve.py
@@ -3,36 +3,44 @@ import re
 with open("input.txt", 'r') as file:
     game_data = file.readlines()
 
-def special(char: str):
-     if char == ".": return False
-     if char == "\n": return False
-     if char.isdigit(): return False
-     print(f"True {char}")
-     return True
+gear_dict = {}
+gear_dict_parts = {}
 
-def is_special(i, j):
+def add_dict(g, word):
+    if g in gear_dict:
+        gear_dict[g].append(int(word))
+    else:
+        gear_dict[g] = [int(word)]
+
+def special(char: str):
+    if char == ".": return False
+    if char == "\n": return False
+    if char.isdigit(): return False
+    print(f"True {char}")
+    return True
+
+def is_special(i, j, word):
     # print(f"special [{i}, {j}]")
     if i < 0 or j < 0:
          return False
     try:
-        return special(game_data[i][j])
-    except:
+        if special(game_data[i][j]):
+            if (game_data[i][j] == "*"):
+                add_dict((i, j), word)
+            return True
         return False
-
-def above_and_below(row, character, word):
-    # print("above below")
-    for i in range(len(word) + 2):
-        if is_special(row-1, character - i): return True
-        if is_special(row+1, character - i): return True
-    return False
+    except Exception:
+        return False
 
 def check(row, character, word):
     # print(f"checking {word=}, at: ({row}, {character})")
-    if (is_special(row, character) |
-        is_special(row, character-len(word)-1) |
-        above_and_below(row, character, word)):
-        return True
-    return False
+    t = 0
+    t += is_special(row, character, word)
+    t += is_special(row, character-len(word)-1, word)
+    for i in range(len(word) + 2):
+        t += is_special(row-1, character - i, word)
+        t += is_special(row+1, character - i, word)
+    return t > 0
 
 cumulator = ""
 all_numbers = list()
@@ -55,6 +63,13 @@ for r, row in enumerate(game_data):
 print(f"{all_numbers}")
 print(f"{sum(all_numbers)=}")
 
+print(f"dict {gear_dict}")
+# foo = list(filter(lambda a: len(gear_dict[a]) == 2, gear_dict))
+foo = {k: v for k, v in gear_dict.items() if len(v) == 2}
+bar = [v[0] * v[1] for k, v in foo.items()]
+print(f"{foo=}")
+print(f"{bar=}")
+print(f"bonus: {sum(bar)}")
 # ;dddddd'
 # ;......'
 # ;.1123.'
